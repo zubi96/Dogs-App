5c6a225d28b45c0bebd2ddf8a6d0632e56937124
Ivan Zubak <ivanzubizubak@gmail.com>
2020-05-18 13:25:36 +0200

    Layout and navigation added

---
+++ app/src/main/java/com/ivanzubak/dogsapp/view/DetailFragment.java
@@ -0,0 +1,42 @@
+package com.ivanzubak.dogsapp.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.ivanzubak.dogsapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DetailFragment extends Fragment {
+    private int dogUuid;
+
+    public DetailFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if(getArguments() != null) {
+            dogUuid = DetailFragmentArgs.fromBundle(getArguments()).getDogUuid();
+        }
+    }
+}
---
+++ app/src/main/java/com/ivanzubak/dogsapp/view/ListFragment.java
@@ -0,0 +1,31 @@
+package com.ivanzubak.dogsapp.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.ivanzubak.dogsapp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ListFragment extends Fragment {
+    public ListFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+}
